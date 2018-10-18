5931b451962902058cd822c4536ec6f90cb1cb48
jarryp <jarry.palacios@hotmail.com>
2018-10-18 10:06:45 -0400

    Inclucion de apellido al web master en footer.php

--- Views/default/footer.php
+++ Views/default/footer.php
@@ -5,7 +5,7 @@
  <p align="center">
  	<font size="2">
  		Todos los Derechos Reservados a <strong>Palacio's Systems</strong><br>
- 		Webmaster: Ing. Jarry Palacios
+ 		Webmaster: Ing. Jarry Palacios Rivas
  	</font>
  </p>
 </body>
